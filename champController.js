a94b3317be4f6ba89d5ff7e15022b3b6e0b58530
GreenKnight15 <jamesritter15@gmail.com>
2015-08-24 10:07:13 -0500

    adding item box features

--- champController.js
+++ champController.js
@@ -100,7 +100,7 @@ return tiles;
                     link:"/stats/"+this.id,
                     aria:this.name,
                     img:"http://ddragon.leagueoflegends.com/cdn/5.15.1/img/item/"+this.image.full,
-                    descLong:this.sanitizedDescription,
+                    descLong:this.sanitizedTooltip,
                     cost:this.gold.total,
 
                     });
@@ -284,13 +284,15 @@ return tiles;
   };
 
 };
-           
+     
 
 	
 })
 
-
-
+.controller('footerController', function($scope){
+	$scope.disclosure = "LoL Item Checker isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.";
+	
+})
 
 
 
