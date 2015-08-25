bde4b1f09851b6361b63e7fb273bcff5d3e7613d
GreenKnight15 <jamesritter15@gmail.com>
2015-08-25 01:22:56 -0500

    debug

--- champController.js
+++ champController.js
@@ -206,16 +206,37 @@ return tiles;
     		  
     	  }
     	  
-    	  if(Object.keys(answer.stats).length == 1){
-    		  $scope.itemStats1.splice(0,1,{
-    			  "statName":key,
-    			  "statValue":value,
-    		  });
+    	  if(Object.keys(answer.stats).length === 1){
+    		  if($scope.itemStats1.length === 0 ){
+    			  $scope.itemStats1.push({
+        			  "statName":key,
+        			  "statValue":value,
+        		  });
+    		  }
+    		  if($scope.itemStats1.length === 1 ){
+    			  $scope.itemStats1.splice(0,1,{
+        			  "statName":key,
+        			  "statValue":value,
+        		  });
+    		  }
+    		  if($scope.itemStats1.length === 2){
+    			  $scope.itemStats1 =[];
+    		  }  
+    		  if($scope.itemStats1.length === 3){
+    			  $scope.itemStats1 =[];
+        		  
+    		  }
     	  }
     	  
     	  
       
     	  if(Object.keys(answer.stats).length === 2){
+    		  if($scope.itemStats1.length === 0 ){
+    			  $scope.itemStats1.push({
+        			  "statName":key,
+        			  "statValue":value,
+        		  });
+    		  }
     		  if($scope.itemStats1.length === 1){
     			  $scope.itemStats1.splice(0,1,{
         			  "statName":key,
@@ -236,6 +257,12 @@ return tiles;
     	  
     	  
     	  if(Object.keys(answer.stats).length === 3){
+    		  if($scope.itemStats1.length === 0 ){
+    			  $scope.itemStats1.push({
+        			  "statName":key,
+        			  "statValue":value,
+        		  });
+    		  }
     		  if($scope.itemStats1.length === 1){
     			  $scope.itemStats1.splice(0,1,{
         			  "statName":key,
