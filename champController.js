c8c2e39f46f42c988c35ee3fe5cfad14b6e106cb
GreenKnight15 <jamesritter15@gmail.com>
2015-08-25 09:17:51 -0500

    debug

--- champController.js
+++ champController.js
@@ -265,8 +265,8 @@ return tiles;
     			  $scope.itemStats1.push({
         			  "statName":key,
         			  "statValue":value,
-        			  console.log("2-3");
         		  });
+    			  console.log("2-3");
     		  }
     	  }
     	  
