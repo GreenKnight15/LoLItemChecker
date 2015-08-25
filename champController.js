075d1b2ee7ee85e36df986f45f1937bef72c932d
GreenKnight15 <jamesritter15@gmail.com>
2015-08-25 18:19:28 -0500

    testing dmg

--- champController.js
+++ champController.js
@@ -220,7 +220,8 @@ angular.module('myApp', ['ngMaterial', 'ngMdIcons', 'ngAria', 'ngRoute', 'ngAnim
 
                     
                     if (names.key === "Damage") {
-                        var dmg = names.value;
+                         var dmg;
+                         Number(dmg) = Number(names.value);
                         console.log("dmg="+dmg);
                         $scope.addedDmg.splice(0, 1, {
                             dmg
