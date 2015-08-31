30d2c7340d30540a7e6430f540aa4b56f232497d
GreenKnight15 <jamesritter15@gmail.com>
2015-08-31 01:06:54 -0500

    tets

--- champController.js
+++ champController.js
@@ -646,28 +646,6 @@ $scope.getItemSet = function() {
 	};
 	}
 	
-	  $scope.showDownload = function(ev) {
-		    // Appending dialog to document.body to cover sidenav in docs app
-		    // Modal dialogs should fully cover application
-		    // to prevent interaction outside of dialog
-		    $mdDialog.show(
-		      $mdDialog.alert()
-		        .parent(angular.element(document.querySelector('#popupContainer')))
-		        .clickOutsideToClose(true)
-		        .title('This is an alert title')
-		        .content('You can specify some description text in here.')
-		        .ariaLabel('Alert Dialog Demo')
-		        .ok('Got it!')
-		        .targetEvent(ev)
-		    );
-		  };
-
-	
-	
-	
-	
-
-	
 	var itemSetName= String($scope.itemSetName);
 	
 
@@ -700,6 +678,19 @@ $scope.getItemSet = function() {
 	}
 		};
 
+		  $scope.showDownload = function(ev) {
+			  
+			    $mdDialog.show(
+			      $mdDialog.alert()
+			        .parent(angular.element(document.querySelector('#popupContainer')))
+			        .clickOutsideToClose(true)
+			        .title('This is an alert title')
+			        .content('You can specify some description text in here.')
+			        .ariaLabel('Alert Dialog Demo')
+			        .ok('Got it!')
+			        .targetEvent(ev)
+			    );
+			  };
 
 
 })
