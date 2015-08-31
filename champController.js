93765ff721b6dc854640d62fb6ae14e507bfed92
GreenKnight15 <jamesritter15@gmail.com>
2015-08-31 00:46:45 -0500

    test

--- champController.js
+++ champController.js
@@ -646,6 +646,22 @@ $scope.getItemSet = function() {
 	};
 	}
 	
+	  $scope.showDownload = function(ev) {
+		    // Appending dialog to document.body to cover sidenav in docs app
+		    // Modal dialogs should fully cover application
+		    // to prevent interaction outside of dialog
+		    $mdDialog.show(
+		      $mdDialog.alert()
+		        .parent(angular.element(document.querySelector('#popupContainer')))
+		        .clickOutsideToClose(true)
+		        .title('This is an alert title')
+		        .content('You can specify some description text in here.')
+		        .ariaLabel('Alert Dialog Demo')
+		        .ok('Got it!')
+		        .targetEvent(ev)
+		    );
+		  };
+
 	
 	
 	
